5fc0941ffddd444e49050ec9cb5ca3cbb5bfb064
Konstantinos Paparas <kelsos86@gmail.com>
2016-02-28 00:02:16 +0200

    Moves the repository interface to a file
    Also adds documentation.

--- mbrc-data/Repository/ITrackRepository.cs
+++ mbrc-data/Repository/ITrackRepository.cs
@@ -11,33 +11,4 @@
     {
         IList<LibraryTrack> GetTracksByAlbumId(long id);
     }
-
-    /// <summary>
-    /// The interface provides a base for the basic actions of a repository.
-    /// </summary>
-    /// <typeparam name="T">The type of data the repository handles</typeparam>
-    public interface IRepository<T>
-    {
-        int Delete(IList<T> t);
-                
-        int SoftDelete(IList<T> t);
-
-        IList<T> GetAll();
-
-        IList<T> GetCached();
-
-        IList<T> GetDeleted();
-
-        T GetById(long id);
-
-        int GetCount();
-
-        IList<T> GetPage(int offset, int limit);
-        
-        IList<T> GetUpdatedPage(int offset, int limit, long epoch);
-        
-        int Save(T t);
-        
-        int Save(IList<T> t);
-    }
 }
