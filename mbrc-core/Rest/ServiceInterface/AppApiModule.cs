7e103b111c4da6eb610c39847ba2f78a10c601ad
Konstantinos Paparas <kelsos86@gmail.com>
2016-05-08 19:38:44 -0700

    Adds basic index path

---
+++ mbrc-core/Rest/ServiceInterface/AppApiModule.cs
@@ -0,0 +1,13 @@
+﻿using MusicBeeRemoteCore.Rest.ServiceModel.Type;
+using Nancy;
+
+namespace MusicBeeRemoteCore.Rest.ServiceInterface
+{
+    internal class AppApiModule : NancyModule
+    {
+        public AppApiModule()
+        {
+            Get["/"] = _ => Response.AsJson(new ResponseBase {Code = ApiCodes.Success});
+        }
+    }
+}
