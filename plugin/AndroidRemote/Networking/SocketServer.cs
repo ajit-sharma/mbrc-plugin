24313413302c5a14b4a2a3b29b17ac85d70afe77
Konstantinos Paparas <kelsos86@gmail.com>
2017-03-02 20:53:50 +0200

    Loopback connections are always accepted

--- plugin/AndroidRemote/Networking/SocketServer.cs
+++ plugin/AndroidRemote/Networking/SocketServer.cs
@@ -211,6 +211,12 @@ namespace MusicBeePlugin.AndroidRemote.Networking
                         isAllowed = true;
                         break;
                 }
+
+                if (IPAddress.IsLoopback(ipAddress))
+                {
+                    isAllowed = true;
+                }
+                
                 if (!isAllowed)
                 {
                     workerSocket.Send(
